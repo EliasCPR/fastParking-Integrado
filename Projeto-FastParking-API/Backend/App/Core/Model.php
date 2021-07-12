7a10658e70c206766b9b37afecc1f72cafcce0c6
Elias Oliveira <eliascpr123@gmail.com>
2021-07-12 09:02:45 -0300

    projeto em andamento

---
+++ Projeto-FastParking-API/Backend/App/Core/Model.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Core;
+
+class Model
+{
+    //Vamos aplicar o padrão de projeto Singleton
+    private static $conexao;
+
+    public static function getConexao()
+    {
+        //se a conexão não estiver criada, criamos ela
+        if(!isset(self::$conexao)){
+            //self é usado para pegar um atributo estáticoo desta classe
+            self::$conexao = new \PDO("mysql:host=localhost;port=3306;dbname=fastParking;", "root", "");
+        }
+
+        //retornamos a connexão
+        return self::$conexao;
+    }
+}
