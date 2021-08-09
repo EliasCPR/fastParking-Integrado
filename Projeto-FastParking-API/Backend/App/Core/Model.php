4fa98d9b75e903ee5e7e70cb6c751bbc40b05f47
20272513 <eliascpr123@gmail.com>
2021-08-09 08:21:11 -0300

    colocando variaveis de ambiente

--- Projeto-FastParking-API/Backend/App/Core/Model.php
+++ Projeto-FastParking-API/Backend/App/Core/Model.php
@@ -9,10 +9,18 @@ class Model
 
     public static function getConexao()
     {
+
+
+
+        $host = $_ENV["database_host"];
+        $user = $_ENV["database_user"];
+        $password = $_ENV["database_pass"]
+
+
         //se a conexão não estiver criada, criamos ela
         if(!isset(self::$conexao)){
             //self é usado para pegar um atributo estáticoo desta classe
-            self::$conexao = new \PDO("mysql:host=localhost;port=3306;dbname=fastParking;", "root", "");
+            self::$conexao = new \PDO("mysql:host=$host;port=3306;dbname=fastParking;", $user, $password);
         }
 
         //retornamos a connexão
